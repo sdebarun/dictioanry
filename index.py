0c75e28df7cf22897b14a644d7fef2e083f81b5d
sdebarun <sdebarun04@gmail.com>
2021-05-28 16:10:52 +0530

    loking for similar words in dictionary

--- index.py
+++ index.py
@@ -1,17 +1,26 @@
-import json
+import json,difflib
 
-#open file in read mode
-f = open('resources/dictionary.json')
-#reading json data in dictionary format
+f = open('resources/data.json')
 dictionary = json.load(f)
-#get input
 word = input("please enter the word : ")
 word = word.lower()  
+
+def checkSimilarity(word):
+    wordlist = []
+    for key in dictionary:
+        wordlist.append(key)
+    similar_words = difflib.get_close_matches(word,wordlist)
+    i=1
+    for similar_word in similar_words:
+        print(f'{i}. {similar_word}')
+        i=i+1
+
 #checking if the word exists
 if word in dictionary.keys():
     print('---------------')
     print(dictionary[word])
 else:
-    print('The word does not exist')    
-    
+    print('The word does not exist. Did you mean any of the following')    
+    #check similarities
+    checkSimilarity(word)
 f.close()    
